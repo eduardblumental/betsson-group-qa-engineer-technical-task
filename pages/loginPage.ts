8b94369bf88e2422aa58a0a605aa23ecdce8c9c7
Eduards Blumentals (TAABLED2) <eduards.blumentals@swisscom.com>
2024-09-11 14:02:08 +0300

    POM for login page created. Login page test ready.

---
+++ pages/loginPage.ts
@@ -0,0 +1,32 @@
+import { Page, Locator } from "@playwright/test";
+
+export class LoginPage {
+  private page: Page;
+  private usernameInput: Locator;
+  private passwordInput: Locator;
+  private loginButton: Locator;
+  private errorMessage: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.usernameInput = page.locator("#user-name");
+    this.passwordInput = page.locator("#password");
+    this.loginButton = page.locator("#login-button");
+    this.errorMessage = page.locator('[data-test="error"]');
+  }
+
+  async goto(): Promise<void> {
+    await this.page.goto("https://www.saucedemo.com/");
+  }
+
+  async login(username: string, password: string): Promise<void> {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+
+  async getErrorMessage(): Promise<string> {
+    const errorMessage = await this.errorMessage.textContent();
+    return errorMessage ? errorMessage : "";
+  }
+}
